ae26b7c72da0939f2a260355a86d7e80328a034f
zachcygan <coding@cyganmail.com>
2026-06-14 22:05:07 -0600

    updated auth

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,3 +1,16 @@
-import { handlers } from "@/auth";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
-export const { GET, POST } = handlers;
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
