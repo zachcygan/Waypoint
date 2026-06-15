3ca458a0eeea4bcd6ccaa4e0b0a0c39555ffd69a
zachcygan <coding@cyganmail.com>
2026-06-14 22:03:48 -0600

    added google auth

---
+++ src/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+});
